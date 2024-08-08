30420d783598697ef4b806cf7d9f6234bb4b050e
Carolynne Padilha da Silva <carol-akiyama@hotmail.com>
2024-08-08 19:05:11 -0300

    add mainevent component

---
+++ src/main-event/MainEvent.tsx
@@ -0,0 +1,13 @@
+import './MainEvent.css';
+
+const MainEvent = () => {
+  return (
+    <>
+      <div id='main-event'>
+        <img src="/img/fifa.jpeg"></img>
+      </div>
+    </>
+  );
+}
+
+export default MainEvent;
